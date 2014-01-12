a57a8d4f91511d624fd70453203f4087d8eebf78
Yann <yann.eugone@gmail.com>
2014-01-12 15:15:14 +0100

    Project homepage + Starting layout implementation

---
+++ src/WillCorp/ZombieBundle/Controller/GlobalController.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * This file is part of the CloudyFlowBundle package
+ *
+ * (c) Yann Eugoné <yann.eugone@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace WillCorp\ZombieBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * @todo: provide class description
+ * 
+ * @author    Yann Eugoné <yann.eugone@gmail.com>
+ * @copyright 2013 © Cloudy-Web-Creations <http://www.cloudy-web-creations.com>
+ */
+class GlobalController
+{
+    /**
+     * @Route("/", name="homepage")
+     * @Template()
+     */
+    public function homepageAction()
+    {
+        return array();
+    }
+}
